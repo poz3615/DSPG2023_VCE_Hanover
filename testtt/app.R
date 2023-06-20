8e148a31ecd8547c2ba286860a89776825ef0492
Wiggins <gwiggins@vt.edu>
2023-06-20 08:53:00 -0400

    add soil graphs

--- testtt/app.R
+++ testtt/app.R
@@ -8,12 +8,42 @@
 #
 
 library(shiny)
+#setting working directory 
+setwd("C:/Users/gwiggins/Documents/hanover-DSPG-2023/data/soil")
+
+#reading in packages
+library(readxl)
+library(ggplot2)
+library(viridis)
+library(sf)
+
+#assigning the data file path to soil excel
+soil_excel <- "C:/Users/gwiggins/Downloads/DSPG2023_VCE_Hanover2/testtt/soillabels.xlsx"
+
+#reading in soil excel and assigning it to soil data
+soil_data <- read_excel(soil_excel)
+
+#pulling just Rating, and Acres in AOI columns from soil data and assigning it to rateacre data
+rateacre_data <- soil_data[, c("Rating", "Acres in AOI")]
+
+#re-naming Rating and Acres in Aoi columns to soil rating and acres
+colnames(rateacre_data) <- c("soil_rating", "acres")
+rateacre_data_clean <- rateacre_data[-c(119,120), ]
+
+#turning soil rating column to a factor to use viridis 
+rateacre_data_clean$soil_rating <- factor(rateacre_data_clean$soil_rating) 
+
+
+
+
+
+
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
 
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
+    titlePanel("Soils In Hanover County"),
 
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
@@ -36,14 +66,18 @@ ui <- fluidPage(
 server <- function(input, output) {
 
     output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
+      #creating a sideways bar plot showing the acres of each rating in the county with viridis
+      ggplot(rateacre_data_clean, aes(x = soil_rating, 
+                                      y = acres, 
+                                      fill = soil_rating)) +
+        geom_bar(stat = "identity") +
+        coord_flip() +
+        scale_fill_viridis_d(option = "viridis") +
+        theme(legend.position = "none") +
+        labs(x = "Soil Rating", 
+             y = "Acreage", 
+             title = "USDA Soil Rating by Acerage in Hanover County", 
+             caption = "Data Source: USDA, NRCC Web Soil Survey, 2019")
     })
 }
 
